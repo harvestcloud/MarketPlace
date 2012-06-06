395d4ad1fb15a595fb4525591dbd69343bca1e6a
Tom Haskins-Vaughan <tom@harvestcloud.com>
2012-06-06 14:03:11 -0400

    Importing SellerBundle/Controller/WelcomeController.php

---
+++ SellerBundle/Controller/WelcomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\MarketPlace\SellerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class WelcomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('HarvestCloudMarketPlaceSellerBundle:Welcome:index.html.twig');
+    }
+}
