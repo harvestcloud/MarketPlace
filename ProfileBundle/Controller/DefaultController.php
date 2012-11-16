3cc7ad0f04e1aa2909edb63de923c302b2f2147d
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-11-15 21:38:39 -0500

    Adding empty ProfileBundle to manage general Profile settings

---
+++ ProfileBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\MarketPlace\ProfileBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('HarvestCloudMarketPlaceProfileBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
