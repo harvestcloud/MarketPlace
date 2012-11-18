02a7303ddf390b8972c1978a6bed2c995a222799
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-11-18 14:56:45 -0500

    Created actions and views to edit Locations

--- ProfileBundle/Controller/LocationController.php
+++ ProfileBundle/Controller/LocationController.php
@@ -10,6 +10,12 @@
 namespace HarvestCloud\MarketPlace\ProfileBundle\Controller;
 
 use HarvestCloud\MarketPlace\ProfileBundle\Controller\ProfileController as Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use HarvestCloud\CoreBundle\Entity\Location;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Form;
+use HarvestCloud\CoreBundle\Form\LocationType;
 
 /**
  * LocationController
@@ -29,6 +35,84 @@ class LocationController extends Controller
     {
         return $this->render('HarvestCloudMarketPlaceProfileBundle:Location:index.html.twig', array(
             'locations' => $this->getCurrentProfile()->getLocations(),
+        ));
+    }
+
+    /**
+     * show
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2012-11-18
+     *
+     * @Route("/location/{id}")
+     * @ParamConverter("location", class="HarvestCloudCoreBundle:Location")
+     *
+     * @param  Location  $location
+     */
+    public function showAction(Location $location)
+    {
+        if ($location->getProfile()->getId() != $this->getCurrentProfile()->getId())
+        {
+            throw new \Exception('No access to this Location');
+        }
+
+        return $this->render('HarvestCloudMarketPlaceProfileBundle:Location:show.html.twig', array(
+          'location' => $location,
+        ));
+    }
+
+    /**
+     * new
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2012-11-18
+     *
+     * @param  Request $request
+     */
+    public function newAction(Request $request)
+    {
+        $location = new Location();
+        $location->setProfile($this->getCurrentProfile());
+        $form = $this->createForm(new LocationType(), $location);
+
+        if ($response = $this->processForm($request, $form, 'Profile_location_show'))
+        {
+            return $response;
+        }
+
+        return $this->render('HarvestCloudMarketPlaceProfileBundle:Location:new.html.twig', array(
+          'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * edit
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2012-11-18
+     *
+     * @Route("/location/{id}")
+     * @ParamConverter("location", class="HarvestCloudCoreBundle:Location")
+     *
+     * @param  Request $request
+     */
+    public function editAction(Location $location, Request $request)
+    {
+        if ($location->getProfile()->getId() != $this->getCurrentProfile()->getId())
+        {
+            throw new \Exception('No access to this Location');
+        }
+
+        $form = $this->createForm(new LocationType(), $location);
+
+        if ($response = $this->processForm($request, $form, 'Profile_location_show'))
+        {
+            return $response;
+        }
+
+        return $this->render('HarvestCloudMarketPlaceProfileBundle:Location:edit.html.twig', array(
+          'form'     => $form->createView(),
+          'location' => $location,
         ));
     }
 }
