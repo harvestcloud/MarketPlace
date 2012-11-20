8750f78af7e288f92588c87425422aa22c20c723
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-11-19 21:10:27 -0500

    Can now edit current Profile

--- ProfileBundle/Controller/DefaultController.php
+++ ProfileBundle/Controller/DefaultController.php
@@ -10,6 +10,8 @@
 namespace HarvestCloud\MarketPlace\ProfileBundle\Controller;
 
 use HarvestCloud\MarketPlace\ProfileBundle\Controller\ProfileController as Controller;
+use Symfony\Component\HttpFoundation\Request;
+use HarvestCloud\CoreBundle\Form\ProfileType;
 
 /**
  * DefaultController
@@ -25,8 +27,33 @@ class DefaultController extends Controller
      * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
      * @since  2012-11-15
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('HarvestCloudMarketPlaceProfileBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('HarvestCloudMarketPlaceProfileBundle:Default:index.html.twig', array(
+            'profile' => $this->getCurrentProfile(),
+        ));
+    }
+
+    /**
+     * edit
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2012-11-18
+     *
+     * @param  Request $request
+     */
+    public function editAction(Request $request)
+    {
+        $form = $this->createForm(new ProfileType(), $this->getCurrentProfile());
+
+        if ($response = $this->processForm($request, $form, 'Profile_homepage'))
+        {
+            return $response;
+        }
+
+        return $this->render('HarvestCloudMarketPlaceProfileBundle:Default:edit.html.twig', array(
+          'form'    => $form->createView(),
+          'profile' => $this->getCurrentProfile(),
+        ));
     }
 }
