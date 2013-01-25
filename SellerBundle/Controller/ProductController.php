1fb04b8b313d59f472c35e1fcd0089af07c3dd2c
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2013-01-24 21:31:03 -0500

    Added ProductController::restockAction

--- SellerBundle/Controller/ProductController.php
+++ SellerBundle/Controller/ProductController.php
@@ -10,9 +10,14 @@
 namespace HarvestCloud\MarketPlace\SellerBundle\Controller;
 
 use HarvestCloud\MarketPlace\SellerBundle\Controller\SellerController as Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use HarvestCloud\CoreBundle\Entity\Product;
+use HarvestCloud\CoreBundle\Entity\RestockTransaction;
 use HarvestCloud\CoreBundle\Form\ProductType;
+use HarvestCloud\CoreBundle\Form\RestockTransactionType;
 
 /**
  * ProductController
@@ -99,6 +104,57 @@ class ProductController extends Controller
 
         return $this->render('HarvestCloudMarketPlaceSellerBundle:Product:new.html.twig', array(
           'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * restock
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2013-01-21
+     *
+     * @Route("/product/{id}/restock")
+     * @ParamConverter("product", class="HarvestCloudCoreBundle:Product")
+     *
+     * @param  Product  $product
+     */
+    public function restockAction(Product $product, Request $request)
+    {
+        $restock = new RestockTransaction();
+        $restock->setProduct($product);
+
+        $form = $this->createForm(new RestockTransactionType(), $restock);
+
+        if ($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
+
+            if ($form->isValid())
+            {
+                try
+                {
+                    // For now, we always post the RestockTransaction, but
+                    // later we'll check to see if this is a future transaction
+                    $restock->post();
+
+                    $em = $this->getDoctrine()->getEntityManager();
+                    $em->persist($restock);
+                    $em->flush();
+
+                    return $this->redirect($this->generateUrl('Seller_product_show', array(
+                        'id' => $product->getId(),
+                    )));
+                }
+                catch (\Exception $e)
+                {
+                    // could not Restock Product
+                }
+            }
+        }
+
+        return $this->render('HarvestCloudMarketPlaceSellerBundle:Product:restock.html.twig', array(
+          'form'    => $form->createView(),
+          'product' => $product,
         ));
     }
 }
