4ab0065244ac708ef73b7cdbdb97d38cd4c0e29e
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2013-03-25 22:40:14 -0400

    Fixed bug that prevents a Buyer being set on an Order as described here: https://trello.com/c/PFa7EGWw

--- BuyerBundle/Controller/BuyerController.php
+++ BuyerBundle/Controller/BuyerController.php
@@ -33,27 +33,35 @@ class BuyerController extends Controller
         // Grab the session
         $session = $this->getRequest()->getSession();
 
+        // Start with an null OrderCollection
+        $orderCollection = null;
+
         // Check if we have a reference to the cart in the session
         if ($session->get('cart_id')) {
             $orderCollection = $this->getRepo('OrderCollection')
                 ->find($session->get('cart_id'));
-
-            // Check if a cart really exists
-            if ($orderCollection) {
-                return $orderCollection;
-            }
         }
 
-        // Cannot find existing cart, create a new one
-        $orderCollection = new OrderCollection();
+        // If we still don't have an OrderCollection, create one and persist it
+        if (!$orderCollection) {
+            $orderCollection = new OrderCollection();
 
-        // Persist new so that we get a 'cart_id'
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($orderCollection);
-        $em->flush();
+            // Persist new so that we get a 'cart_id'
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($orderCollection);
+            $em->flush();
 
-        // Save new cart to session
-        $session->set('cart_id', $orderCollection->getId());
+            // Save new cart to session
+            $session->set('cart_id', $orderCollection->getId());
+        }
+
+        // If the current User is fully authenticated, set the current Profile
+        // as the Buyer. If the user is not yet fully authenticated, then the
+        // Buyer will be assigned at login
+        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $user = $this->get('security.context')->getToken()->getUser();
+            $orderCollection->setBuyer($user->getCurrentProfile());
+        }
 
         return $orderCollection;
     }
